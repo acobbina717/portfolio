151e815260df7f9db977e03c746d9ba03d7bbde3
Abeku <acobbina717@gmail.com>
2022-12-09 04:04:01 -0500

    darkMode Theme & Navigation Header

---
+++ src/Components/HeaderNav/HeaderNav.tsx
@@ -0,0 +1,61 @@
+import { ActionIcon, Burger, Container, Group, Header } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+import DarkMode from "../DarkMode/DarkMode";
+import { useStyles } from "./header-nav.styles";
+
+import { IconBrandLinkedin, IconBrandGithub } from "@tabler/icons";
+
+interface HeaderNavProps {
+  links: { link: string; label: string }[];
+}
+
+const HeaderNav = ({ links }: HeaderNavProps) => {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+
+  return (
+    <Header height={56} mb={120}>
+      <Container className={classes.inner}>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          size="sm"
+          className={classes.burger}
+        />
+
+        <Group className={classes.links} spacing={5}>
+          {links.map((link, idx) => (
+            <a
+              key={idx}
+              href={link.link}
+              className={cx(classes.link, {
+                [classes.linkActive]: active === link.link,
+              })}
+              onClick={(e) => {
+                e.preventDefault();
+                setActive(link.link);
+              }}
+            >
+              {link.label}
+            </a>
+          ))}
+        </Group>
+
+        <DarkMode size={28} />
+
+        <Group spacing={0} className={classes.social} position="right" noWrap>
+          <ActionIcon size="lg">
+            <IconBrandLinkedin size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <IconBrandGithub size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </Header>
+  );
+};
+
+export default HeaderNav;
